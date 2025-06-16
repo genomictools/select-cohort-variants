f36fc01e214f7d1eaca17950ad27afe06cbd8461
mahmed03 <mahmoud.ahmed@icr.ac.uk>
2025-06-16 16:15:44 +0100

    full pipeline tests

--- test-submit.sh
+++ test-submit.sh
@@ -10,24 +10,24 @@
 module load Java/17
 source $NXF_CONDA
 
-# # Setup tests
-# curl -fsSL https://get.nf-test.com | bash
-# ./nf-test init
-# ./nf-test generate pipeline main.nf
+# Setup tests
+curl -fsSL https://get.nf-test.com | bash
+./nf-test init
+./nf-test generate pipeline main.nf
 
-# # Download input data
-# mkdir -p tests tests/input
-# URL="https://figshare.com/ndownloader/files"
-# wget -c $URL/50487621 -O tests/input/pheno.variants.vcf.gz
-# wget -c $URL/50487624 -O tests/input/pheno.variants.vcf.gz.tbi
-# wget -c $URL/50385591 -O tests/input/pheno.cases.txt
+# Download input data
+mkdir -p tests tests/input
+URL="https://figshare.com/ndownloader/files"
+wget -c $URL/50487621 -O tests/input/pheno.variants.vcf.gz
+wget -c $URL/50487624 -O tests/input/pheno.variants.vcf.gz.tbi
+wget -c $URL/50385591 -O tests/input/pheno.cases.txt
 
-# # Create input file
-# echo "cohort,chrom,start,end,file,index,samples" > input/cohorts_input.csv
-# echo "pheno1,,,,$PWD/tests/input/pheno.variants.vcf.gz,$PWD/tests/input/pheno.variants.vcf.gz.tbi,$PWD/tests/input/pheno.cases.txt" >> input/cohorts_input.csv
-# echo "pheno2,chr2,,,$PWD/tests/input/pheno.variants.vcf.gz,$PWD/tests/input/pheno.variants.vcf.gz.tbi,$PWD/tests/input/pheno.cases.txt" >> input/cohorts_input.csv
-# echo "pheno3,chr3,,,$PWD/tests/input/pheno.variants.vcf.gz,$PWD/tests/input/pheno.variants.vcf.gz.tbi,$PWD/tests/input/pheno.cases.txt" >> input/cohorts_input.csv
-# echo "pheno4,chr3,1,20000000,$PWD/tests/input/pheno.variants.vcf.gz,$PWD/tests/input/pheno.variants.vcf.gz.tbi,$PWD/tests/input/pheno.cases.txt" >> input/cohorts_input.csv
+# Create input file
+echo "cohort,chrom,start,end,file,index,samples" > tests/input/cohorts_input.csv
+echo "pheno1,,,,$PWD/tests/input/pheno.variants.vcf.gz,$PWD/tests/input/pheno.variants.vcf.gz.tbi,$PWD/tests/input/pheno.cases.txt" >> tests/input/cohorts_input.csv
+echo "pheno2,chr2,,,$PWD/tests/input/pheno.variants.vcf.gz,$PWD/tests/input/pheno.variants.vcf.gz.tbi,$PWD/tests/input/pheno.cases.txt" >> tests/input/cohorts_input.csv
+echo "pheno3,chr3,,,$PWD/tests/input/pheno.variants.vcf.gz,$PWD/tests/input/pheno.variants.vcf.gz.tbi,$PWD/tests/input/pheno.cases.txt" >> tests/input/cohorts_input.csv
+echo "pheno4,chr3,1,20000000,$PWD/tests/input/pheno.variants.vcf.gz,$PWD/tests/input/pheno.variants.vcf.gz.tbi,$PWD/tests/input/pheno.cases.txt" >> tests/input/cohorts_input.csv
 
 # Run tests
 ./nf-test test tests/main.nf.test
